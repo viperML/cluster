0a58951946c42b6939b1b4e282c982ab14dfb040
Fernando Ayats <fayats@bsc.es>
2025-09-25 11:22:52 +0200

    Port forwarding

--- default.nix
+++ default.nix
@@ -14,33 +14,59 @@ let
         }
         <nixpkgs/nixos/modules/virtualisation/qemu-vm.nix>
         {
-          virtualisation.sharedDirectories = lib.mkForce {
-            nix-store = {
-              source = builtins.storeDir;
-              target = "/nix/.ro-store";
-              securityModel = "none";
+
+          virtualisation = {
+
+            sharedDirectories = lib.mkForce {
+              nix-store = {
+                source = builtins.storeDir;
+                target = "/nix/.ro-store";
+                securityModel = "none";
+              };
             };
+
+            graphics = false;
+
+            # qemu.options = [
+            #   "-device usb-net,netdev=net0"
+            #   "-netdev user,id=net0,hostfwd=tcp::2222-:22"
+            # ];
           };
-          virtualisation.graphics = false;
         }
       ];
     }
   ) machines;
 
   json = pkgs.formats.json { };
+
   config = json.generate "process-compose.yml" {
     version = "0.5";
     log_level = "debug";
-    processes = builtins.mapAttrs (name: nixos: {
-      command = pkgs.writeShellScript "run-${name}" ''
-        set -x
-        IMAGES_DIR="$PWD/images"
-        mkdir -p "$IMAGES_DIR"
-        export NIX_DISK_IMAGE="$IMAGES_DIR/${name}.qcow2"
-        exec ${lib.getExe nixos.config.system.build.vm}
-      '';
 
-    }) all-nixos;
+    processes = builtins.listToAttrs (
+      lib.imap0 (
+        i:
+        { name, value }:
+        lib.nameValuePair name {
+          command = lib.getExe (
+            pkgs.writeShellApplication {
+              name = "command";
+              runtimeInputs = [ pkgs.coreutils ];
+              inheritPath = false;
+              text = ''
+                IMAGES_DIR="$PWD/images"
+                mkdir -p "$IMAGES_DIR"
+                export NIX_DISK_IMAGE="$IMAGES_DIR/${name}.qcow2"
+                export QEMU_NET_OPTS="hostfwd=tcp::2222${toString i}-:22"
+                export QEMU_OPTS="-net nic,model=virtio -net socket,mcast=230.0.0.1:1234"
+                set -x
+                exec ${lib.getExe value.config.system.build.vm}
+              '';
+            }
+          );
+        }
+      ) (lib.attrsToList all-nixos)
+    );
   };
 in
 
@@ -49,7 +75,15 @@ in
   inherit config;
   inherit pkgs;
 
-  run = pkgs.writeShellScriptBin "run" ''
-    ${lib.getExe pkgs.process-compose}
-  '';
+  run = pkgs.writeShellApplication {
+    name = "run";
+    runtimeInputs = [
+      pkgs.process-compose
+      pkgs.bash
+    ];
+    inheritPath = false;
+    text = ''
+      process-compose -t=false -f ${config}
+    '';
+  };
 }
--- machines.nix
+++ machines.nix
@@ -7,25 +7,32 @@ let
       isNormalUser = true;
       createHome = true;
       openssh.authorizedKeys.keys = [
-        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAxks+8Jg1PDy8W+4mPLN9G0LUYp5bscEk7MOm+WtzrlfQilflbY8BIbyvxNteGjxuO0u78YXCjJSYiZxQAkt9k= ayats hermes tpm2"
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCyqWuElqeIjZyAEpp9se0+MrTlxSKIwPUh/ccmv60muXD8mCz0bOLKVwgmMjBVgPpt4ED8VqKkeX4NXzsvdiWueM1YEGgqVQSpZ9UqvEsob6ugZX3eWkGvcFShkCQcfcL0gVo2dCem/WefchAC0BWHT403QxnCRwfDQSKO5ipvdkru+zZZ7IHxvpvJVqjBtZ4KFiApbEokne2w1tMGXjaWifCw00gdUYTOgn5z0PILx12cLzLqh8Q64zeyl3v0ATtz2StQPJJNEPemHyfjQkRw4MsEjYnp7NnREDmUGjQ68DvAIbiqTzYPY2Ju7GtX3J7n0IvJpkfhNFM9z+z1p1Mh ayats BSC-8488104251 tpm2"
       ];
+      extraGroups = ["wheel"];
     };
+
+    services.getty.autologinUser = "nixos";
+
+    networking.firewall.enable = false;
+
+    security.sudo.wheelNeedsPassword = false;
   };
 
   imports = [ shared ];
 in
 {
+  lab0 =
+    { config, pkgs, ... }:
+    {
+      inherit imports;
+      networking.hostName = "lab0";
+    };
+
   lab1 =
     { config, pkgs, ... }:
     {
       inherit imports;
       networking.hostName = "lab1";
-    };
-
-  lab2 =
-    { config, pkgs, ... }:
-    {
-      inherit imports;
-      networking.hostName = "lab2";
     };
 }
