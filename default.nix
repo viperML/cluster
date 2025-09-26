4fddc229e28fdc0a43791eae5c3ba80a2958d898
Fernando Ayats <fayats@bsc.es>
2025-09-26 12:37:24 +0200

    Tweak scripts

--- default.nix
+++ default.nix
@@ -6,11 +6,15 @@ let
 
   machines = import ./machines.nix;
 
-  nMachines = lib.length (builtins.attrNames machines);
+  nMachines =
+    let
+      n = lib.length (builtins.attrNames machines);
+    in
+    lib.throwIf (n > 256) "Too many machines" n;
 
   toHexPadded = number: lib.fixedWidthString 2 "0" (lib.toHexString number);
 
-  ipFor = i: "192.168.100.${toString (i + 10)}";
+  ipFor = i: "192.168.100.${toString i}";
 
   configs = builtins.listToAttrs (
     lib.imap0 (
@@ -27,7 +31,9 @@ let
           ];
           networking.hostName = lib.mkForce name;
           networking.hosts = builtins.listToAttrs (
-            lib.imap0 (j: name: lib.nameValuePair (ipFor j) [ name ]) (builtins.attrNames machines)
+            lib.imap0 (j: name: lib.nameValuePair (ipFor j) ((lib.mkIf (i != j) [ name ]))) (
+              builtins.attrNames machines
+            )
           );
           networking.interfaces.eth1.ipv4 = {
             addresses = [
@@ -42,13 +48,13 @@ let
             qemu.networkingOptions = lib.mkForce (
               [
                 # Forward SSH
-                "-device virtio-net,netdev=mynet${toString i},mac=52:54:00:12:34:${toHexPadded i}"
+                "-device virtio-net,netdev=mynet${toString i},mac=52:54:00:00:00:${toHexPadded i}"
                 "-netdev user,id=mynet${toString i},hostfwd=tcp::${toString (22220 + i)}-:22"
               ]
               ++ (lib.optionals (nMachines > 1) [
                 # Internal connection
-                "-device virtio-net,netdev=net0,mac=52:54:00:12:35:${toHexPadded i}"
-                "-netdev socket,id=net0,mcast=230.0.0.1:1234,localaddr=127.0.0.1"
+                "-device virtio-net,netdev=net0,mac=52:54:00:00:01:${toHexPadded i}"
+                "-netdev socket,id=net0,mcast=230.0.0.1:50821,localaddr=127.0.0.1"
               ])
             );
             sharedDirectories = lib.mkForce {
@@ -68,20 +74,13 @@ let
   process-compose-config = (pkgs.formats.json { }).generate "process-compose.yml" {
     version = "0.5";
     processes = builtins.mapAttrs (name: nixos: {
-      command = lib.getExe (
-        pkgs.writeShellApplication {
-          name = "command";
-          runtimeInputs = [ pkgs.coreutils ];
-          inheritPath = false;
-          text = ''
-            IMAGES_DIR="$PWD/images"
-            mkdir -p "$IMAGES_DIR"
-            export NIX_DISK_IMAGE="$IMAGES_DIR/${name}.qcow2"
-            set -x
-            exec ${lib.getExe nixos.config.system.build.vm}
-          '';
-        }
-      );
+      command = pkgs.writeShellScript "start-${name}" ''
+        IMAGES_DIR="$PWD/images"
+        mkdir -p "$IMAGES_DIR"
+        export NIX_DISK_IMAGE="$IMAGES_DIR/${name}.qcow2"
+        set -x
+        exec ${lib.getExe nixos.config.system.build.vm}
+      '';
     }) configs;
   };
 in
@@ -90,15 +89,13 @@ in
 
   inherit configs process-compose-config pkgs;
 
-  run = pkgs.writeShellApplication {
-    name = "run";
-    runtimeInputs = [
-      pkgs.process-compose
-      pkgs.bash
-    ];
-    inheritPath = false;
-    text = ''
-      process-compose -t=false -f ${process-compose-config}
-    '';
-  };
+  run = pkgs.writeShellScriptBin "run-cluster" ''
+    export PATH='${
+      lib.makeBinPath [
+        pkgs.process-compose
+        pkgs.bash
+      ]
+    }'
+    exec process-compose -t=false -f ${process-compose-config}
+  '';
 }
